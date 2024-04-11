e141f7ce8e7bb9840d1715524e33185c3ca67e6b
Jiyoon <163503035+itsjiyoon@users.noreply.github.com>
2024-04-11 18:56:14 +0900

    Deleted useless variables and add comments

--- rps.js
+++ rps.js
@@ -1,74 +1,79 @@
-let rock = "rock";
-let paper = "paper";
-let scissors = "scissors";
-let userInput;
-
-function getComputerChoice() {
+function getComputerChoice() {  // Generate computer choice and return the choice
     let result = Math.floor(Math.random() * 3);
 
     if (result == 0) {
-        console.log(rock);
+        console.log("rock");
+
+        return "rock";
     }
 
     else if (result == 1) {
-        console.log(paper);
+        console.log("paper");
+
+        return "paper";
     }
 
     else { 
-        console.log(scissors);
-    }
+        console.log("scissors");
 
-    function getUserChoice() {
-        userInput = toLowerCase(prompt("rock? paper? scissors? :D"));
+        return "scissors";
     }
+} 
 
-    function  playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock") {
-            switch (computerSelection) {
-                case "paper" :
-                    console.log("you lose! " + computerSelection + " beats " + playerSelection);
-                    break;
+
+function getUserChoice() { // Ask user to put their choice and return the choice
+    let userInput = toLowerCase(prompt("rock? paper? scissors? :D"));
+
+    return userInput;
+}
+
+
+function  playRound(playerSelection, computerSelection) { // Play one round based on computer's choice and user's choice
+    if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "paper" :
+                console.log("you lose! " + computerSelection + " beats " + playerSelection);
+                break;
                 
-                case "scissors" :
-                    console.log("you win! " + playerSelection + " beats " + computerSelection);
-                    break;
+            case "scissors" :
+                console.log("you win! " + playerSelection + " beats " + computerSelection);
+                break;
                 
-                default :
-                    console.log("tied!");
-                    break;
-            }
+            default :
+                console.log("tied!");
+                break;
         }
-
-        else if (playerSelection === "paper") {
-            switch (computerSelection) {
-                case "scissors" :
-                    console.log("you lose! " + computerSelection + " beats " + playerSelection);
-                    break;
-                
-                case "rock" :
-                    console.log("you win! " + playerSelection + " beats " + computerSelection);
-                    break;
-                
-                default :
-                    console.log("tied! ");
-                    break;
-            }
-        }
-
-        else {
-            switch (computerSelection) {
-                case "rock" :
-                    console.log("you lose! " + computerSelection + " beats " + playerSelection);
-                    break;
-                
-                case "paper" :
-                    console.log("you win! " + playerSelection + " beats " + computerSelection);
-                    break;
-                
-                default :
-                    console.log("tied!");
-                    break;
-            }
-        }       
     }
+
+    else if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "scissors" :
+                console.log("you lose! " + computerSelection + " beats " + playerSelection);
+                break;
+                
+            case "rock" :
+                console.log("you win! " + playerSelection + " beats " + computerSelection);
+                break;
+                
+            default :
+                console.log("tied! ");
+                break;
+        }
+    }
+
+    else {
+        switch (computerSelection) {
+            case "rock" :
+                console.log("you lose! " + computerSelection + " beats " + playerSelection);
+                break;
+                
+            case "paper" :
+                console.log("you win! " + playerSelection + " beats " + computerSelection);
+                break;
+                
+            default :
+                console.log("tied!");
+                break;
+        }
+    }       
 }
